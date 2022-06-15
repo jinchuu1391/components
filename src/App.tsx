4c747ca66485842aca07e88121e978f41f562257
Jinsoo Park <jinchuu1391@gmail.com>
2022-06-15 14:34:45 +0900

    chore: project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+ <div className='bg-orange-500 w-full h-10'>test</div>
+    </div>
+  )
+}
+
+export default App
---
+++ storybook-static/assets/preview.009a67b6.js
@@ -0,0 +1,2 @@
+const t={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{t as parameters};
+//# sourceMappingURL=preview.009a67b6.js.map
